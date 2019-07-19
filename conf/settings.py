819de275df21a949ff0f79ca189784e440eb6e49
Bram <cortex@worlddomination.be>
2019-07-19 15:51:32 +0200

    [fix] debug should never be set to true in production for security reasons

--- conf/settings.py
+++ conf/settings.py
@@ -3,8 +3,8 @@
 # (N.B : the site root is also related to the nginx conf!)
 SITE_ROOT = "__YNH_APP_WEBPATH__"
 
-DEBUG = True
-TEMPLATE_DEBUG = True
+DEBUG = False
+TEMPLATE_DEBUG = False
 
 #APP_FSPATH = "__YNH_APP_FSPATH"
 
