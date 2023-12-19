7ba01e16c08a0364b56e7532ec984caab32a14d7
Gildas <25692645+Gildas-GH@users.noreply.github.com>
2023-12-19 17:55:25 +0100

    Test subfolder and sso

--- conf/__init__.py
+++ conf/__init__.py
@@ -1,7 +1,25 @@
-from flask import Flask
+from flask import Flask, Blueprint, request
 
-app = Flask(__name__)
+# Use Blueprints to automatically use the app subfolder
+bp = Blueprint('main', __name__, url_prefix='__PATH__')
 
-@app.route("/")
+# Define your routes
+@bp.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+# Exemple of SSO use
+@bp.route("/user")
+def user():
+    if request.headers.get('Auth-User'):
+        id = request.headers.get('Auth-User')
+        name = request.headers.get('Name')
+        email = request.headers.get('Email')
+
+        return f'<p>Hello, {name}! Your ID is {id} and e-mail is {email}.</p>'
+    else:
+        return "<p>Hello, Anonymous!</p>"
+
+# After defining all routes, create Flask app and register Blueprint.
+app = Flask(__name__)
+app.register_blueprint(bp)
