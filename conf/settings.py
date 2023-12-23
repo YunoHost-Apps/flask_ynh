7dca6c6ac003b48165c9a46f4c3aac72de969b2d
Gildas <25692645+Gildas-GH@users.noreply.github.com>
2023-12-23 15:10:02 +0100

    Setup database

--- conf/settings.py
+++ conf/settings.py
@@ -1,3 +1,8 @@
 # Site path contains the subfolder configured during the YunoHost
 # app install. It is changed automatically during URL change.
 SITE_PATH="__PATH__"
+
+# Database configuration
+DB_USER="__DB_USER__"
+DB_NAME="__DB_NAME__"
+DB_PASS="__DB_PWD__"
